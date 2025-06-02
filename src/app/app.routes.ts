66939a2b19018a67c064f89a5c97890e92b9dbbb
ewurasi nyarkoa <anewurasi@gmail.com>
2025-06-02 16:21:31 +0000

    Style invoice detail page to match design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { InvoiceListComponent } from '../app/components/invoice-list/invoice-list.component';
+import { InvoiceDetailComponent } from './components/invoice-detail/invoice-detail.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/invoices', pathMatch: 'full' },
   { path: 'invoices', component: InvoiceListComponent },
+  { path: 'invoices/:id', component: InvoiceDetailComponent },
   { path: '**', redirectTo: '/invoices' }
 ];
